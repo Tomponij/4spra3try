d64899f351d93ba8c3c2b8e0c69980ce2ef01d6e
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2021-12-21 10:27:32 +0100

    games page aangepast
    ik heb de games page zodanig aangepast dat je ziet welk veld ze moten spelen en wat de stand was

--- resources/views/pages/games.blade.php
+++ resources/views/pages/games.blade.php
@@ -2,54 +2,52 @@
 
 @section('content')
     <table class="table table-danger">
-        <h1>Nog te spelen toernooien</h1>
+        <h1>Aankomende Wedstrijden</h1>
         <thead>
         <tr>
             <th scope="col">Tijd</th>
+            <th scope="col">Veld</th>
             <th scope="col">Thuis team</th>
-            <th scope="col">Goals thuis team</th>
             <th scope="col">vs</th>
             <th scope="col">Uit team</th>
-            <th scope="col">Goals uit team</th>
-
+            <th scope="col">Stand</th>
         </tr>
         </thead>
         <tbody>
         @foreach($teams as $team)
             <tr>
                 <td>{{$team->start_time}}</td>
+                <td>{{$team->field_id}}</td>
                 <td>{{$team->name}}</td>
-                <td>{{$team->team1_score}}</td>
                 <td>vs</td>
                 <td>{{$team->name}}</td>
-                <td>{{$team->team1_score}}</td>
+                <td>{{$team->team1_score}} - {{$team->team2_score}}</td>
             </tr>
         @endforeach
         </tbody>
     </table>
 
     <table class="table table-danger">
-        <h1>Gespeelden toernooien</h1>
+        <h1>Gespeelden Wedstrijden</h1>
         <thead>
         <tr>
             <th scope="col">Tijd</th>
+            <th scope="col">Veld</th>
             <th scope="col">Thuis team</th>
-            <th scope="col">Goals thuis team</th>
             <th scope="col">vs</th>
             <th scope="col">Uit team</th>
-            <th scope="col">Goals uit team</th>
-
+            <th scope="col">Stand</th>
         </tr>
         </thead>
         <tbody>
         @foreach($teams as $team)
             <tr>
-                <th scope="row">{{$team->start_time}}</th>
+                <td>{{$team->start_time}}</td>
+                <td>{{$team->field_id}}</td>
                 <td>{{$team->name}}</td>
-                <td>{{$team->team1_score}}</td>
                 <td>vs</td>
                 <td>{{$team->name}}</td>
-                <td>{{$team->team1_score}}</td>
+                <td>{{$team->team1_score}} - {{$team->team2_score}}</td>
             </tr>
         @endforeach
         </tbody>
